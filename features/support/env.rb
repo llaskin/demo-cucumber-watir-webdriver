7f6e4c76b4572ec3243a8fd7b023d912cfcd9589
Leo Laskin <leolaskin@Leos-MacBook-Pro.local>
2016-03-21 15:04:40 -0700

    fixing to use the proper environment variables

--- features/support/env.rb
+++ features/support/env.rb
@@ -25,7 +25,7 @@ Before do | scenario |
     :name => "#{scenario.feature.name} - #{scenario.name}"
   }
 
-  url = "http://#{ENV['TESTER_SAUCE_USERNAME']}:#{ENV['TESTER_SAUCE_ACCESS_KEY']}@ondemand.saucelabs.com:80/wd/hub".strip
+  url = "http://#{ENV['SAUCE_USERNAME']}:#{ENV['SAUCE_ACCESS_KEY']}@ondemand.saucelabs.com:80/wd/hub".strip
   puts @build
   client = Selenium::WebDriver::Remote::Http::Default.new
   client.timeout = 180
