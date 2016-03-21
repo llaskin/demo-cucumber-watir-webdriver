98a67bab84b660544edabca96f947d40d3346ebb
Leo Laskin <leolaskin@Leos-MacBook-Pro.local>
2016-03-21 15:07:47 -0700

    removing debugging statement that is unneeded

--- features/support/env.rb
+++ features/support/env.rb
@@ -26,14 +26,13 @@ Before do | scenario |
   }
 
   url = "http://#{ENV['SAUCE_USERNAME']}:#{ENV['SAUCE_ACCESS_KEY']}@ondemand.saucelabs.com:80/wd/hub".strip
-  puts @build
   client = Selenium::WebDriver::Remote::Http::Default.new
   client.timeout = 180
   @browser = Watir::Browser.new(:remote, :url => url, :desired_capabilities => capabilities_config, :http_client => client)
 end
 
 After do | scenario |
-  sleep 3
+  sleep 1
   sessionid = @browser.driver.send(:bridge).session_id
   jobname = "#{scenario.feature.name} - #{scenario.name}"
 
